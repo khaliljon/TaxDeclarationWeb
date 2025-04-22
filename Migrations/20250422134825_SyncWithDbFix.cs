c0e373de6dd4218a6484b7d21137cb33362eae53
khaliljon <khalil_jon@mail.ru>
2025-04-22 18:55:26 +0500

    error 1770

---
+++ Migrations/20250422134825_SyncWithDbFix.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaxDeclarationWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncWithDbFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Инспекторы_InspectorId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_InspectorId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InspectorId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InspectorId",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_InspectorId",
+                table: "AspNetUsers",
+                column: "InspectorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Инспекторы_InspectorId",
+                table: "AspNetUsers",
+                column: "InspectorId",
+                principalTable: "Инспекторы",
+                principalColumn: "инспектор",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
